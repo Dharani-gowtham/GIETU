1dfd8a5de57a5102c70745f417049f9307958d44
Dharani Gowtham S <dharaniadithya1998.da@gmail.com>
2024-02-19 08:16:31 +0530

    rebuild

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
 import streamlit as st
-<<<<<<< HEAD
 st.set_page_config(layout="wide")
 
 st.title("Python Basics, OOPS & Data Structures")
-=======
-st.set_page_config(layout="wide")
->>>>>>> 58d001f17aed93af35c8fa2f7792295bc0f3429b
