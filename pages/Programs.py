df0e56ab7a07d79efd3244ff3a60c8cb0515676d
Dharani Gowtham S <dharaniadithya1998.da@gmail.com>
2024-03-01 15:26:43 +0530

    empty

---
+++ pages/Programs.py
@@ -0,0 +1,38 @@
+# import streamlit as st
+
+# st.code("""
+# for _ in range(1000):
+#     print("Hello World")
+#     """)
+
+# def add(x):
+#     return x+x
+
+# data = [1,2,3,4,5,6]
+# result = list(map(add,data))
+# print(result)
+
+# a, b = map(int, input("Enter data: ").split())
+# print(a, b, type(a), type(b))
+
+
+# data = list(map(int, input("Enter the Data: ").split()))
+# n = 100
+# result = (n*(n+1)/2) - sum(data)
+# print(result)
+
+# num = 12334542
+# print(str(num)[::-1])
+
+# a, b, c, d = map(int, input("Data: ").split())
+# print(a,b,c,d)
+
+# children = 10
+# candies_with_chef = 3
+# each_packer = 4
+# remain_need_to_buy = children - candies_with_chef
+# final_packets = 0
+# if (remain_need_to_buy % 4) != 0:
+#     final_packets += 1
+# print(final_packets + remain_need_to_buy // 4)
+
