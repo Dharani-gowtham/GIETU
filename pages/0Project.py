e84d84caac5ac5e2396990c4afd2bd21b971c61a
Dharani Gowtham S <dharaniadithya1998.da@gmail.com>
2024-03-14 11:12:47 +0530

    Updated Pass percent

--- pages/0Project.py
+++ pages/0Project.py
@@ -1,13 +1,14 @@
 import streamlit as st
 
 st.title("Project")
-st.subheader("Marks Allocated")
+st.subheader("Marks Allocated: Pass percentage: 60")
 data = {
     "Problem Statement Description": "2",
     "Problem Solution Description": "2",
     "Steps to Achieve that Solution": "6",
     "Code": "3",
-    "Working": "7"
+    "Working": "7",
+    "Total": "20"
         }
 
 st.data_editor(data, use_container_width=True)
