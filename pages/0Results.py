6b97fdc44e378b7737bf006cec95d70ecf284a6d
Dharani Gowtham S <dharaniadithya1998.da@gmail.com>
2024-03-16 16:04:14 +0530

    Added browse Table

--- pages/0Results.py
+++ pages/0Results.py
@@ -6,5 +6,8 @@ st.set_page_config(layout="wide")
 batch1 = pd.read_csv("batch1.csv")
 
 st.title("Project Result")
+
+if st.button("Browse Roll"):
+    st.data_editor(batch1, hide_index=True, use_container_width=True, disabled=("BATCH", "NAME", "ROLL NUMBER", "P-Desc", "S-Desc", "C-Desc", "CODE", "WORKING" , "TOTAL MARKS"))
+    
 st.table(batch1)
-# st.data_editor(batch1, hide_index=True, use_container_width=True, disabled=("BATCH", "NAME", "ROLL NUMBER", "P-Desc", "S-Desc", "C-Desc", "CODE", "WORKING" , "TOTAL MARKS"))
