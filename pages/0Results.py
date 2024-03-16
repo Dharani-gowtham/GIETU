459ce7b8439938b818cd469f2b6218241c023886
Dharani Gowtham S <dharaniadithya1998.da@gmail.com>
2024-03-16 11:06:01 +0530

    Changed to Table format

--- pages/0Results.py
+++ pages/0Results.py
@@ -6,4 +6,5 @@ st.set_page_config(layout="wide")
 batch1 = pd.read_csv("batch1.csv")
 
 st.title("Project Result")
-st.data_editor(batch1, hide_index=True, use_container_width=True)
+st.table(batch1)
+# st.data_editor(batch1, hide_index=True, use_container_width=True, disabled=("BATCH", "NAME", "ROLL NUMBER", "P-Desc", "S-Desc", "C-Desc", "CODE", "WORKING" , "TOTAL MARKS"))
